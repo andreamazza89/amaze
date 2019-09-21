8f5518264ada32d8184bad4f800c7730cd3ca177
andrea mazzarella <andrea@andreas-MacBook-Pro.local>
2019-09-21 09:02:49 +0200

    Agamennon sends Apollo home

---
+++ frontend/src/elm/Main/index.d.ts
@@ -0,0 +1,22 @@
+// WARNING: Do not manually modify this file. It was generated using:
+// https://github.com/dillonkearns/elm-typescript-interop
+// Type definitions for Elm ports
+
+export namespace Elm {
+  namespace Main {
+    export interface App {
+      ports: {
+        tellMeSeconds: {
+          subscribe(callback: (data: string) => void): void
+        }
+        secondsReceived: {
+          send(data: string): void
+        }
+      };
+    }
+    export function init(options: {
+      node?: HTMLElement | null;
+      flags: null;
+    }): Elm.Main.App;
+  }
+}

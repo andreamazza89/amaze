8e18508711d2cc1d0a3219ed0213bab3f8b2612b
andrea mazzarella <andrea.mazzarella89@gmail.com>
2020-03-29 22:43:39 +0200

    adds the other maze

--- src/main/kotlin/com/andreamazzarella/amaze/core/Game.kt
+++ src/main/kotlin/com/andreamazzarella/amaze/core/Game.kt
@@ -10,7 +10,7 @@ import com.andreamazzarella.amaze.utils.pipe
 
 data class Game(
     val id: GameId = generateGameId(),
-    val maze: Maze = aMazeFromADrawing(DEFAULT_MAZE),
+    val maze: Maze = aMazeFromADrawing(OTHER_MAZE),
     private val players: List<Player> = emptyList()
 ) {
 

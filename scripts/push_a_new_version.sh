1c6378d9d7d8babc2b8457b0c526877453b8d3f8
andrea mazzarella <andrea.mazzarella89@gmail.com>
2020-04-11 15:45:30 +0200

    updates the docker build script

--- scripts/push_a_new_version.sh
+++ scripts/push_a_new_version.sh
@@ -4,10 +4,12 @@ set -eu
 
 VERSION_TAG=$1
 
-## Login with ECR
+echo "Don't forget to actually build a new version of the app (build the frontend and the jar)"
+
+echo "Loggin into ECR"
 aws ecr get-login-password --region eu-west-1 \
   | docker login --username AWS --password-stdin 556103293623.dkr.ecr.eu-west-1.amazonaws.com
 
-## Build the image and push it up
+echo "Building the image and then pushing it up"
 docker build -t 556103293623.dkr.ecr.eu-west-1.amazonaws.com/amaze:$VERSION_TAG .
 docker push 556103293623.dkr.ecr.eu-west-1.amazonaws.com/amaze:$VERSION_TAG
